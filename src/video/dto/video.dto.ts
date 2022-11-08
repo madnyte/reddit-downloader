84c04386fdabd1b52c2b3c11790aa15165af77b1
motheo <motheo.ken@gmail.com>
2022-11-08 17:05:44 +0200

    add class validators to video request models

--- src/video/dto/video.dto.ts
+++ src/video/dto/video.dto.ts
@@ -1,12 +1,21 @@
 // RedditRequest
+
+import { IsNotEmpty, IsUrl } from 'class-validator';
+
 // a request to get reddit url response as json and return video urls
 export class RedditRequest {
+  @IsUrl()
+  @IsNotEmpty()
   url: string;
 }
 
 // A request to the server with a reddit video link
 // and / or audio link to merge the 2.
 export class VideoRequest {
+  @IsUrl()
+  @IsNotEmpty()
   url: string;
+
+  @IsUrl()
   audio?: string;
 }
