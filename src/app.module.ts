0b7af731f84c38d0c9b33953b8f81d4fd600cdd4
motheo <motheo.ken@gmail.com>
2022-11-08 15:20:47 +0200

    add video module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { VideoModule } from './video/video.module';
+import { VideoController } from './video/video.controller';
+import { VideoService } from './video/video.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [VideoModule],
+  controllers: [VideoController],
+  providers: [VideoService],
 })
 export class AppModule {}
